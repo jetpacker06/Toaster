77b5398f6b2bb4a2e3fde30916a24c735c0207c8
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2022-02-22 18:09:59 -0600

    working on fixing toaster texture glitch and hitbox

--- src/main/java/com/jetpacker06/toaster/Toaster.java
+++ src/main/java/com/jetpacker06/toaster/Toaster.java
@@ -2,6 +2,8 @@ package com.jetpacker06.toaster;
 
 import com.jetpacker06.toaster.block.ModBlocks;
 import com.jetpacker06.toaster.item.ModItems;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
@@ -10,6 +12,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
@@ -36,7 +39,13 @@ public class Toaster {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
-//Comment
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.TOASTER.get(), RenderType.cutout());
+    }
+
+
+
     private void setup(final FMLCommonSetupEvent event) {
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
